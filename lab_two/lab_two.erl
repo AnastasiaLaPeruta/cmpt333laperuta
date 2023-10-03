498aa15df423075f15a616848805dbcb3ce4eb52
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2023-10-03 01:55:44 +0000

    started lab two in erlang

---
+++ lab_two/lab_two.erl
@@ -0,0 +1,17 @@
+%
+% Lab Two - Recursion
+% By Anastasia.
+%
+
+-module(lab_two).
+-export([listoflists/2]).
+
+%
+% -- Public -- 
+%
+%listoflists(listLength,number) -> number * fac1(listLength-1);
+%listoflists(_) -> 1. % base case
+
+
+
+   
