45dad613090660532daf0d349493076a73c38b6a
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2023-10-03 20:29:18 +0000

    trying out different approaches

--- lab_two/lab_two.erl
+++ lab_two/lab_two.erl
@@ -4,14 +4,31 @@
 %
 
 -module(lab_two).
--export([listoflists/2]).
+-export([listoflists/3]).
 
 %
 % -- Public -- 
 %
-%listoflists(listLength,number) -> number * fac1(listLength-1);
-%listoflists(_) -> 1. % base case
 
+%listoflists(valueInc,numLists,numElements) -> makeSmallerList(valueInc,numLists,numElements),
+%listoflists(valueInc,numLists-1,numElements);
+%listoflists(_,0,_) -> io:fwrite("~w",[finalList]). % base case
+
+%
+% -- Private --
+%
+
+%newList(valueInc,numLists,numElements) -> [].
+%increasingNum() -> numLists.
+%makeSmallerList(valueInc,numLists,numElements) -> newList(valueInc,numLists,numElements) ++ [increasingNum()],
+%makeSmallerList(valueInc,increasingNum,numElements-1);
+%makeSmallerList(_,_,0) -> newList(valueInc,numLists,numElements). % base case
+list() -> [].
+listoflists([Head|Tail]) -> determineNum(list) ++ []
+
+
+determineNum(num) when is_integer(num) -> num + m;
+determineNum(_) -> io:fwrite("Not a valid value").
 
 
    
