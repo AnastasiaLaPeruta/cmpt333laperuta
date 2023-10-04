1037b040f7b16f6604187a9e9e4afb69e4a1d796
AnastasiaLaPeruta <89801518+AnastasiaLaPeruta@users.noreply.github.com>
2023-10-04 23:52:40 +0000

    completed more of it

--- lab_two/lab_two.erl
+++ lab_two/lab_two.erl
@@ -18,11 +18,10 @@ big_list() -> [].
 % this public function is meant to grab straightforward information and start up our private functions
 % which will do all the heavy lifting
 start_off(Counter, Element) -> 
-
     % assigns variables that will be passed to the recursive function 
+    Default = Element, % allows final_list to access to reset
     Start_num = Counter, 
-    sm_list() ++ [Start_num], % adds starting number before it gets to function that will increment it
-    inner_list(sm_list, Start_num, Counter, Element). % starts recursive call for inner list
+    inner_list(sm_list() ++ [Start_num], Start_num, Counter, Element, Default). % starts recursive call for inner list
 
 
 
@@ -30,12 +29,13 @@ start_off(Counter, Element) ->
 % -- Private--
 %
 
-inner_list(_,_,_,0) -> final_list(sm_list, Start_num - 1, Counter); % base case for when elements of list have all been inserted
+inner_list(smlist, Start_num, Counter, 0, Default) -> final_list(smlist, Start_num - 1, Counter, 0, Default); % base case for when elements of list have all been inserted
 
 % decrease element left by one each time and recursively calls itself to add the next number to the small list
-inner_list(sm_list, Start_num, Counter, Element) -> sm_list() ++ [inner_list(sm_list, Start_num + Counter, Counter, Element - 1)].
+inner_list(smlist, Start_num, Counter, Element, Default) -> smlist ++ [inner_list(smlist, Start_num + Counter, Counter, Element - 1, Default)].
 
 
 % recursive function that will add all of the smaller lists into one large one
-final_list(sm_list, 0, Counter) -> io:fwrite(big_list() ++ [sm_list],"~w"); % base case for when the correct amount of lists has been added
-final_list(sm_list, Start_num, Counter) -> io:fwrite("This second one works").
+final_list(smlist, 0, Counter, Element, Default) -> io:fwrite(big_list() ++ [smlist],"~w"); % base case for when the correct amount of lists has been added
+final_list(smlist, Start_num, Counter, Element, Default) -> big_list() ++ [smlist], % adds small list to the big list
+inner_list([Start_num - 1], Start_num - 1, Counter, Element, Default). % resets small list and adds first value
